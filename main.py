c80a989415f4ee49c3e52a5add573ce7e4d25385
SHdow NNN <70070584+Sayyednaa@users.noreply.github.com>
2020-11-25 11:19:32 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+from art import *
+
+import os
+print("<==============Text To ASCII Art Converer. ==========>\n")
+print("Coded By:\n")
+Art=text2art("Abdul Ali",font='block',chr_ignore=True) # Return ASCII text with block font
+print(Art)
+user = ''
+while user != '~':
+    user = input("Enter The Input or ~ to Quit:\n")
+
+    if user =='~':
+        
+        exit()
+    Art=text2art(user,font='block',chr_ignore=True) # Return ASCII text with block font
+    file = open(user +".txt","w+")
+    file.write(Art)
+    print(Art)
+
